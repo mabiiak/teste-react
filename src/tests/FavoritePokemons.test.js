9c1caf374465e9ee9535ba4709c1a5f23448c0eb
mabiane <mabiane.polniak@gmail.com>
2021-12-03 17:01:46 -0300

    add //src/renderWithRouter.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemon from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste qual o texto é exibido.', () => {
+    // Acessar os elementos da tela ----> screen
+    render(<FavoritePokemon />);
+    // Interagir com os elementos
+    const textNotFound = screen.getByText(/No favorite pokemon found/i);
+    // Fazer os testes ----> jest
+    expect(textNotFound).toBeInTheDocument();
+
+
+  });
+});
