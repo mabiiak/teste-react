04b2bf689f5a375ef3155e66bad8f7df39fa2d1e
mabiane <mabiane.polniak@gmail.com>
2021-12-06 19:26:26 -0300

    wip Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,6 +3,9 @@ import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
+import Pokemons from '../data';
+
+const types = ['All', 'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
 
 describe('Teste o componente <Pokedex.js />', () => {
   beforeEach(() => {
@@ -19,30 +22,33 @@ describe('Teste o componente <Pokedex.js />', () => {
     const buttonNext = screen.getByText(/próximo pokémon/i);
     expect(buttonNext).toBeInTheDocument();
 
-    userEvent.click(buttonNext);
-    const namePokemon = screen.getByText(/charmander/i); // segundo pokemon
-
-    expect(namePokemon).toBeInTheDocument();
-
-  // O primeiro Pokémon da lista deve ser mostrado ao clicar no botão, se estiver no último Pokémon da lista;
+    Pokemons.forEach((pokemon) => {
+      const name = screen.getByText(pokemon.name);
+      expect(name).toBeInTheDocument();
+      userEvent.click(buttonNext);
+    });
   });
 
   test('Teste se é mostrado apenas um Pokémon por vez.', () => {
-  // tentar puxar pela classe da div (o card do pokemon) ---> contar tamanho dos elementos? quantia de filhos?
-  // const {container} = render(<MyComponent />)
-  // const foo = container.querySelector('[data-foo="bar"]') */
-    const textCounter = screen.getByText(/More details/i);
+    const img = screen.getByRole('img');
 
-    console.log(textCounter);
-    expect(textCounter.length === 2);
+    expect(img).toBeDefined(); // ATENÇÃO
   });
 
-  // test('Teste se a Pokédex tem os botões de filtro.', () => {
-  // // Deve existir um botão de filtragem para cada tipo de Pokémon, sem repetição.
-  // // A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo;
-  // // O texto do botão deve corresponder ao nome do tipo, ex. Psychic;
-  // // O botão All precisa estar sempre visível.
-  // });
+  test('Teste se a Pokédex tem os botões de filtro.', () => {
+    types.forEach((type) => {
+      const buttonType = screen.getByRole('button', { name: type });
+      expect(buttonType).toBeInTheDocument();
+    });
+
+    // A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo;
+
+    // O texto do botão deve corresponder ao nome do tipo, ex. Psychic;
+
+    // O botão All precisa estar sempre visível.
+    const buttonAll = screen.getByRole('button', { name: 'All' });
+    expect(buttonAll).toBeVisible();
+  });
 
   // test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
   // // O texto do botão deve ser All;
